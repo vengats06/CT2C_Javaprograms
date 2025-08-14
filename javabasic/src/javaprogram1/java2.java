2f160dde7eea33275cd83c59a9f4e4ccd3b962b2
STUDENT <STUDENT@DESKTOP-PJSUP61>
2025-08-14 11:34:41 +0530

    second

---
+++ javabasic/src/javaprogram1/java2.java
@@ -0,0 +1,23 @@
+package programs1;
+import java.io.*;
+import java.util.*;
+
+public class add {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner s = new Scanner(System.in);  // Unused, can be removed
+
+        System.out.println("Enter the values:");
+        int a, b, c;
+
+        a = Integer.parseInt(br.readLine());
+        b = Integer.parseInt(br.readLine());
+        c = a + b;
+
+        System.out.println("Sum is: " + c);
+
+
+	}
+
+}
