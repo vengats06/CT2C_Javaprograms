878d37a0794f78839d46af10916fba2c3c3d052a
STUDENT <STUDENT@DESKTOP-PJSUP61>
2025-08-26 11:55:47 +0530

    day 12 userdefined

---
+++ javabasic/src/day12/userdefineexp/InvalidMarksException.java
@@ -0,0 +1,14 @@
+package day12.userdefineexp;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
